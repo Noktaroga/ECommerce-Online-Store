c3ed57f938c8052d4cd8bd443b07bf21c6746169
Noktaroga <joseglagunar@gmail.com>
2023-05-11 02:04:15 -0400

    Cambiada la configuracion de db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,20 @@
-const Sequelize = require('sequelize');
+/*const Sequelize = require('sequelize');
 const sequelize = new Sequelize('amazonassc_db', 'durottar', '060665cc', {
   host: 'localhost',
   dialect: 'postgres',
+});
+
+module.exports = sequelize;*/
+
+
+//Heroku
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 module.exports = sequelize;
